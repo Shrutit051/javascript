3999b7d860c4c3a72099ea64f3924468409eeda0
Shrutit051 <shruti.thakur@cumminscollege.in>
2026-01-18 16:33:16 +0530

    clock added

---
+++ clock/script.js
@@ -0,0 +1,19 @@
+// const clock = document.getElementById('clock');
+// // const clock = document.querySelector('#clock')
+
+// setInterval(function () {
+//   let date = new Date();
+//   // console.log(date.toLocaleTimeString());
+//   clock.innerHTML = date.toLocaleTimeString();
+// }, 1000);
+
+
+
+const clock=document.getElementById('clock');
+//const clock =document.querySelector('#clock')
+// console.log(clock);
+
+setInterval(function(){
+    let date=new Date()
+    clock.innerHTML=date.toLocaleTimeString()
+}, 1000);
